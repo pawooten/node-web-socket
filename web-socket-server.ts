8b3296e556f036a5ffcf6334afad5dcb3d8048d7
Paul Wooten <pawooten@gmail.com>
2017-07-05 09:07:34 -0400

    Added express initialization within web-socket-server.ts for serving node_modules static content

--- web-socket-server.ts
+++ web-socket-server.ts
@@ -1,14 +1,11 @@
 import * as express from "express";
 import * as path from "path";
+import {Server} from "ws";
 
 const app = express();
 
-app.use('/', function (request, response, next ) {
-  console.log('app.use() invoked for /');
-  //Process the next middleware request
-  next();
-})
 app.use('/', express.static(path.join(__dirname, '..', 'static')));
+app.use('/node_modules', express.static(path.join(__dirname, '..', 'node_modules')));
 
 // app.get('/', (request, response) => {
 //     response.sendFile(path.join(__dirname, '../static/hello-world.html'));
