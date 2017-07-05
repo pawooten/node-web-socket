4e9c18a8780196112458d4515be63c6e265ca426
Paul Wooten <pawooten@gmail.com>
2017-07-05 08:53:52 -0400

    Added static/index.html, updated web-socket-server.ts to server page, added start and start-dev scripts to package.json

--- web-socket-server.ts
+++ web-socket-server.ts
@@ -3,13 +3,11 @@ import * as path from "path";
 
 const app = express();
 
-// app.use('/', function (request, response, next ) {
-//   console.log('app.use() invoked for /');
-//   Process the next middleware request
-//   next();
-
-//   response.sendFile(path.join(__dirname, '../static/hello-world.html'));
-// })
+app.use('/', function (request, response, next ) {
+  console.log('app.use() invoked for /');
+  //Process the next middleware request
+  next();
+})
 app.use('/', express.static(path.join(__dirname, '..', 'static')));
 
 // app.get('/', (request, response) => {
